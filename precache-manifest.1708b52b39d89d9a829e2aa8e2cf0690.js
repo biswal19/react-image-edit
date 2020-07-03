757f9157ae16725bb68f22179c0e21a410a58787
biswal19 <chandrikabiswal1@gmail.com>
2020-07-03 12:24:14 -0700

    Updates

---
+++ precache-manifest.1708b52b39d89d9a829e2aa8e2cf0690.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1a821f655b1121e9ad60a4f6ce87f5",
+    "url": "/react-image-edit/index.html"
+  },
+  {
+    "revision": "a5dbe286a06645583765",
+    "url": "/react-image-edit/static/css/2.33f97271.chunk.css"
+  },
+  {
+    "revision": "f07761b49ef17f6c3311",
+    "url": "/react-image-edit/static/css/main.12506ae0.chunk.css"
+  },
+  {
+    "revision": "a5dbe286a06645583765",
+    "url": "/react-image-edit/static/js/2.1945c955.chunk.js"
+  },
+  {
+    "revision": "2901baf57747062bcf45ab726832dae1",
+    "url": "/react-image-edit/static/js/2.1945c955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07761b49ef17f6c3311",
+    "url": "/react-image-edit/static/js/main.58522ee8.chunk.js"
+  },
+  {
+    "revision": "11e899e24f16e0ef3b82",
+    "url": "/react-image-edit/static/js/runtime-main.c5ed3f31.js"
+  },
+  {
+    "revision": "10ec5171edb1beda4fa06a894382c5c2",
+    "url": "/react-image-edit/static/media/lotus.10ec5171.jpg"
+  }
+]);
